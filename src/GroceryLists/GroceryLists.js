f446bfcfea7b7fef87d9ffcc019b0abb0e326c53
Darlene McDaniel <darlenemcdaniel@Darlenes-MacBook-Pro.local>
2019-12-10 01:34:29 -0500

    Refactore GroceryLists file

--- src/GroceryLists/GroceryLists.js
+++ src/GroceryLists/GroceryLists.js
@@ -12,51 +12,30 @@ import ListService from '../Services/ListService';
 
 
 export default class GroceryLists extends Component {
-  static defaultProps = {
-    match: {
-      params: {}
-    }
-  }
+  
+ 
 
   static contextType = ListContext;
-  render() {
-    const { listId } = this.props.match.params;
-    const { lists=[] } = this.context;
-    const groceryLists = ListService.getLists();
-    console.log(groceryLists);
 
-    return (
-      
-        <>
-        <header>
-            <h2>Grocery Lists</h2>
-        </header>
-      <section className='GroceryLists'>
-        <ul id="grocery__lists">
-          {groceryLists.map(list =>
-            <li key={list.id}>
+  componentDidMount() {
+    this.context.clearError()
+    ListService.getLists()
+      .then(this.context.setList)
+      .catch(this.context.setError)
+  }
+  render() {
+    const { list = [] } = this.context;
+
+    return ( 
+      list.map(list =>
                 <GroceryList
                 id={list.id}
                 name={list.name}
                 items={list.items}
                 aria-controls="groceryList__list"
                 className='groceryList__grocery-link'
+                />
                 
-              />
-              
-              
-              
-            </li>
-          )}
-        </ul>
-      </section>
-      </>
     )
-  }
-
+    )}
 }
-
-
-GroceryLists.propType = {
-  match: PropTypes.object.isRequired
-};
