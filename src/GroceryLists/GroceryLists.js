3c0e58c06b560b538ef00942a874489eef2c9dc7
Darlene McDaniel <darlenemcdaniel@Darlenes-MacBook-Pro.local>
2019-12-14 18:49:50 -0500

    Lists GET fully functional

--- src/GroceryLists/GroceryLists.js
+++ src/GroceryLists/GroceryLists.js
@@ -5,6 +5,7 @@ import './GroceryLists.css'
 import ListContext from '../Contexts/ListContext'
 import GroceryList  from '../IndividualGroceryList/IndividualGroceryList'
 import ListService from '../Services/ListService';
+import ContainerIndividualGroceryList from '../Containers/IndividualListContainer'
 
 
 export default class GroceryLists extends Component {
@@ -25,6 +26,7 @@ export default class GroceryLists extends Component {
       
       lists.map(list =>
                 <GroceryList
+                id={list.id}
                 key={list.id}
                 name={list.name}
                 aria-controls="groceryList__list"
