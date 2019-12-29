884b7239035dc35c137987b2358c3efd93c369c1
Linden Daniels <lindendaniels@gmail.com>
2019-12-29 14:54:37 -0500

    change recipeUrl

--- src/Forms/AddRecipeForm/AddRecipeForm.js
+++ src/Forms/AddRecipeForm/AddRecipeForm.js
@@ -19,37 +19,37 @@ class AddRecipe extends React.Component {
         folderSelectValid: false,
         validationMessage: null
     };
-    
+
     static contextType = FolderContext;
 
     componentDidMount() {
-  
+
         FolderService.getFolders()
-          .then(this.context.setFolder)
-          .catch(this.context.setError) 
-      }
-      
-   
-  
+            .then(this.context.setFolder)
+            .catch(this.context.setError)
+    }
+
+
+
 
     goBack = () => {
         this.props.history.goBack();
     }
 
-    updateFormEntry(e) {       
+    updateFormEntry(e) {
         const name = e.target.name;
         const value = e.target.value;
         let id;
         if (e.target.selectedOptions) {
             id = e.target.selectedOptions[0].id;
             this.setState({
-                'folderId': id 
+                'folderId': id
             })
         }
         this.setState({
             [e.target.name]: e.target.value,
-            
-        }, () => {this.validateEntry(name, value)});
+
+        }, () => { this.validateEntry(name, value) });
     }
 
     validateEntry(name, value) {
@@ -59,39 +59,41 @@ class AddRecipe extends React.Component {
         if ((name === 'name') || (name === 'ingredients') || (name === 'instructions')) {
             if (value.length < 1) {
                 hasErrors = true
-            } 
+            }
 
             else {
                 hasErrors = false
             }
         }
-        
+
         else if ((name === 'folderSelect') && (value === 'Select')) {
             hasErrors = true
         }
-        
+
         else {
             hasErrors = false
         }
-        
+
         this.setState({
             [`${name}Valid`]: !hasErrors,
-        }, this.formValid );
+        }, this.formValid);
     }
 
     formValid() {
         const { nameValid, instructionsValid, ingredientsValid, folderSelectValid } = this.state;
-        if (nameValid && instructionsValid && ingredientsValid && folderSelectValid === true){
+        if (nameValid && instructionsValid && ingredientsValid && folderSelectValid === true) {
             this.setState({
                 formValid: true,
                 validationMessage: null
             });
         }
-        else {this.setState({
-            formValid: !this.formValid,
-            validationMessage: 'All fields are required.'
-        })}
-      }
+        else {
+            this.setState({
+                formValid: !this.formValid,
+                validationMessage: 'All fields are required.'
+            })
+        }
+    }
 
     handleSubmit(e) {
         e.preventDefault();
@@ -102,10 +104,11 @@ class AddRecipe extends React.Component {
             ingredients: ingredients,
             folderid: folderId,
         }
-       
-        this.setState({error: null})
 
+        this.setState({ error: null })
         
+
+
         fetch(`${config.API_ENDPOINT}/recipes`, {
             method: 'POST',
             body: JSON.stringify(recipe),
@@ -113,157 +116,150 @@ class AddRecipe extends React.Component {
                 'content-type': 'application/json'
             }
         })
-        .then(res => {
-            if (!res.ok) {
-                return res.json().then(err => {
-                    console.log(`Error is: ${err}`)
-                    throw err
-                })
-            }
-            return res.json()
-        })
-        .then(data => {
-           
-            this.context.addRecipe(data)
-            
-            
-            const recipeUrl = `/recipes/${folderId}`
-           
-            this.props.history.push(recipeUrl)
-            console.log(this.props.history)
-            
-
-
-        this.setState({ error: null })
-        })
-        .catch(err => {
-            this.setState({ err })
-        })
+            .then(res => {
+                console.log(res.ok)
+                if (!res.ok) {
+                    return res.json().then(err => {
+                        console.log(`Error is: ${err}`)
+                        throw err
+                    })
+                }
+                return res.json()
+            })
+            .then(data => {
+                //this.context.addRecipe(data)
+                const recipeUrl = `/recipes/${data.folderId}`
+                this.props.history.push(recipeUrl);
+                this.setState({ error: null })
+            })
+            .catch(err => {
+                this.setState({ err })
+            })
     }
-    
-    
+
+
     render() {
         const { folders = [] } = this.context;
-       
+
         const options = folders.map((folder) => {
             return (
-            <option 
-                key ={folder.id} 
-                id={folder.id}>
-            {folder.name}
-            </option>
+                <option
+                    key={folder.id}
+                    id={folder.id}>
+                    {folder.name}
+                </option>
             )
         })
         return (
             <>
-            <header>
-                <h2>Add Recipe</h2>
-            </header>
-           
-            <form 
-                className="addrecipe-form"
-                onSubmit={e => this.handleSubmit(e)}>
-                <h2 className="title">Add Recipe</h2>
-                <div className="form-section">
-                  <label htmlFor="name">Recipe Name</label>
-                  <input 
-                    type="text" 
-                    className="field"
-                    name="name" 
-                    id="name" 
-                    aria-label="Name"
-                    aria-required="true"
-                    placeholder="Recipe Name"
-                    onChange={e => this.updateFormEntry(e)}/>
-                </div>
-                <div className="form-section">
-                   <label htmlFor="content">Instructions</label>
-                   <textarea 
-                        className="field"
-                        name="instructions" 
-                        id="instructions"
-                        aria-label="Instructions"
-                        aria-required="false"
-                        onChange={e => this.updateFormEntry(e)}/>
-                </div>
-                <div className="form-section">
-                  <label htmlFor="ingredients">Ingredients</label>
-                  <input 
-                    type="text" 
-                    className="field"
-                    name="ingredients" 
-                    id="ingredients" 
-                    aria-label="ingredients"
-                    aria-required="true"
-                    placeholder="Almond Milk"
-                    onChange={e => this.updateFormEntry(e)}/>
-                    <input 
-                    type="text" 
-                    className="field"
-                    name="ingredients" 
-                    id="ingredients-1" 
-                    aria-label="ingredients"
-                    aria-required="true"
-                    placeholder="Almond Milk"
-                    onChange={e => this.updateFormEntry(e)}/>
-                    <input 
-                    type="text" 
-                    className="field"
-                    name="ingredients" 
-                    id="ingredients-2" 
-                    aria-label="ingredients"
-                    aria-required="true"
-                    placeholder="Almond Milk"
-                    onChange={e => this.updateFormEntry(e)}/>
-                    <input 
-                    type="text" 
-                    className="field"
-                    name="ingredients" 
-                    id="ingredients-3" 
-                    aria-label="ingredients"
-                    aria-required="true"
-                    placeholder="Almond Milk"
-                    onChange={e => this.updateFormEntry(e)}/>
-                    <input 
-                    type="text" 
-                    className="field"
-                    name="ingredients" 
-                    id="ingredients-4" 
-                    aria-label="ingredients"
-                    aria-required="true"
-                    placeholder="Almond Milk"
-                    onChange={e => this.updateFormEntry(e)}/>
-                </div>
-                <div className="form-section">
-                  <label htmlFor="folder-select">folder</label>
-                  <select 
-                    type="text" 
-                    className="field"
-                    name="folderSelect" 
-                    id="folder-select" 
-                    aria-label="folder"
-                    aria-required="true"
-                    ref={this.folderSelect}
-                    onChange={e => this.updateFormEntry(e)}>
-                        <option>Select</option>
-                        { options }
-                    </select>
-                </div>
-                <div className="buttons">
-                 <button 
-                    type="button" 
-                    className="button"
-                    onClick={()=> this.goBack()}>
-                     Cancel
+                <header>
+                    <h2>Add Recipe</h2>
+                </header>
+
+                <form
+                    className="addrecipe-form"
+                    onSubmit={e => this.handleSubmit(e)}>
+                    <h2 className="title">Add Recipe</h2>
+                    <div className="form-section">
+                        <label htmlFor="name">Recipe Name</label>
+                        <input
+                            type="text"
+                            className="field"
+                            name="name"
+                            id="name"
+                            aria-label="Name"
+                            aria-required="true"
+                            placeholder="Recipe Name"
+                            onChange={e => this.updateFormEntry(e)} />
+                    </div>
+                    <div className="form-section">
+                        <label htmlFor="content">Instructions</label>
+                        <textarea
+                            className="field"
+                            name="instructions"
+                            id="instructions"
+                            aria-label="Instructions"
+                            aria-required="false"
+                            onChange={e => this.updateFormEntry(e)} />
+                    </div>
+                    <div className="form-section">
+                        <label htmlFor="ingredients">Ingredients</label>
+                        <input
+                            type="text"
+                            className="field"
+                            name="ingredients"
+                            id="ingredients"
+                            aria-label="ingredients"
+                            aria-required="true"
+                            placeholder="Almond Milk"
+                            onChange={e => this.updateFormEntry(e)} />
+                        <input
+                            type="text"
+                            className="field"
+                            name="ingredients"
+                            id="ingredients-1"
+                            aria-label="ingredients"
+                            aria-required="true"
+                            placeholder="Almond Milk"
+                            onChange={e => this.updateFormEntry(e)} />
+                        <input
+                            type="text"
+                            className="field"
+                            name="ingredients"
+                            id="ingredients-2"
+                            aria-label="ingredients"
+                            aria-required="true"
+                            placeholder="Almond Milk"
+                            onChange={e => this.updateFormEntry(e)} />
+                        <input
+                            type="text"
+                            className="field"
+                            name="ingredients"
+                            id="ingredients-3"
+                            aria-label="ingredients"
+                            aria-required="true"
+                            placeholder="Almond Milk"
+                            onChange={e => this.updateFormEntry(e)} />
+                        <input
+                            type="text"
+                            className="field"
+                            name="ingredients"
+                            id="ingredients-4"
+                            aria-label="ingredients"
+                            aria-required="true"
+                            placeholder="Almond Milk"
+                            onChange={e => this.updateFormEntry(e)} />
+                    </div>
+                    <div className="form-section">
+                        <label htmlFor="folder-select">folder</label>
+                        <select
+                            type="text"
+                            className="field"
+                            name="folderSelect"
+                            id="folder-select"
+                            aria-label="folder"
+                            aria-required="true"
+                            ref={this.folderSelect}
+                            onChange={e => this.updateFormEntry(e)}>
+                            <option>Select</option>
+                            {options}
+                        </select>
+                    </div>
+                    <div className="buttons">
+                        <button
+                            type="button"
+                            className="button"
+                            onClick={() => this.goBack()}>
+                            Cancel
                  </button>
-                 <button 
-                    type="submit" 
-                    className="button"
-                    disabled={!this.state.formValid}>
-                     Save
+                        <button
+                            type="submit"
+                            className="button"
+                            disabled={!this.state.formValid}>
+                            Save
                  </button>
-                </div>
-            </form> 
+                    </div>
+                </form>
             </>
         )
     }
