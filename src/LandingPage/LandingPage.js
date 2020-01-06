7b4ec6382c170546b1daab38cbb5a8034536556b
Linden Daniels <lindendaniels@gmail.com>
2020-01-05 21:03:56 -0500

    Update landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -18,21 +18,21 @@ export default function LandingPage() {
         <header>
             <h3>Meal Planning and a Grocery List</h3>
         </header>
-        <p><img src="https://i.ibb.co/NY76hNF/Screen-Shot-2019-12-31-at-11-13-18-AM.png" alt="recipe interface"/></p>
+        <img src="https://i.ibb.co/xg5wT8x/Screen-Shot-2020-01-05-at-9-00-19-PM.png" alt="recipe interface"/>
         <p>Grocery Plus allows you to add recipes, and add grocery and non-grocery items to a grocery list.</p>
       </section>
       <section className="landing-section">
         <header>
             <h3>All-In-One Grocery List</h3>
         </header>
-        <p><img src="https://i.ibb.co/k5yLcZ5/Screen-Shot-2019-12-31-at-11-17-21-AM.png" alt="a grocery list"></img></p>
+        <img src="https://i.ibb.co/5kq45CJ/Screen-Shot-2020-01-05-at-9-02-44-PM.png" alt="a grocery list"></img>
         <p>Now you can do all your shopping without having to switch apps!</p>
       </section>
       <section className="landing-section">
         <header>
             <h3>Add Your Own Recipes</h3>
         </header>
-        <p><img src="https://i.ibb.co/TqxQRwP/Screen-Shot-2019-12-31-at-11-19-09-AM.png" alt="Add recipe interface"></img></p>
+        <img src="https://i.ibb.co/Rjx7mrx/Screen-Shot-2020-01-05-at-8-53-14-PM.png" alt="Add recipe interface"></img>
         <p>Add your own recipes!</p>
       </section>
 
